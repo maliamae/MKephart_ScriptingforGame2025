5aaa5f0ca698fe5e40bc4d712cb68201b944c2d6
maliamae <165858570+maliamae@users.noreply.github.com>
2025-02-17 17:37:44 -0800

    banging my head in over the counter

--- Assets/Scripts/Week 4/HW Bobomb/CounterManager.cs
+++ Assets/Scripts/Week 4/HW Bobomb/CounterManager.cs
@@ -1,7 +1,11 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CounterManager : MonoBehaviour
 {
+    public GameObject counterDisplay;
+    int count = 0;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +15,11 @@ public class CounterManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKey(KeyCode.A))
+        {
+            count +=1;
+
+            counterDisplay.GetComponent<TMPro.TextMeshPro>().text = "Kill Me";
+        }
     }
 }
